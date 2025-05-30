324ad2fc8528c6e708b3ce87c014b77b10a4e587
Jacob <jacobmbaldwin@gmail.com>
2025-05-29 19:59:57 -0500

    Nav Snackbar message

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-nav-bar',
@@ -14,5 +15,12 @@ import { MatIconModule } from '@angular/material/icon';
   styleUrl: './nav-bar.component.css'
 })
 export class NavBarComponent {
+  constructor(private snackBar: MatSnackBar) {}
 
+  showNotImplemented(): void {
+    this.snackBar.open("Ope! That doesn't work yet!", 'Close', {
+      duration: 3000,
+      verticalPosition: 'bottom', // or 'top'
+    });
+  }
 }
